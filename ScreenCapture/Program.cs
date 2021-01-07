55ddab848969f8da30cd4df21d110a61ab088e4d
Nilesh <it.nilesh.25@gmail.com>
2021-01-07 22:44:29 +0530

    init screen capture solution

---
+++ ScreenCapture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace ScreenCapture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Thread.Sleep(1000);
+                //ScreenShot.ScreenCapture sc = new ScreenShot.ScreenCapture();
+                //sc.CaptureScreenToFile($"{new Random().Next(10)}.jpeg", ImageFormat.Jpeg);
+
+                Bitmap bt = CaptureScreen.CS.GetDesktopImage();
+                bt.Save($"{new Random().Next(10)}.jpeg", ImageFormat.Jpeg);
+            }
+        }
+    }
+}
